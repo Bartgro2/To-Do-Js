9d75679161f0ae509ec192d83565c17c4a186603
Bart <akiraenishi@gmail.com>
2024-10-15 22:23:41 +0200

    Working with modules - E6 (Guide Js)

--- index.js
+++ index.js
@@ -1,27 +1,11 @@
 // inheritance
 
-class Person {
-  constructor(name) {
-    this.name = name;
-  }
-  walk() {
-    console.log("walk");
-  }
-}
-
-class Teacher extends Person {
-  constructor(name, degree) {
-    super(name);
-    this.degree = degree;
-  }
-  teach() {
-    console.log("teach");
-  }
-}
+import { Teacher } from './teacher';
 
 const teacher = new Teacher('John', 'MSc');
 teacher.teach();
 teacher.walk();
+
 
 
 
