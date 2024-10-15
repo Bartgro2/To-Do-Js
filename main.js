0a320b5abc7c21c814e1d6d5c0ac114f17a8fe85
Bart <akiraenishi@gmail.com>
2024-10-15 12:09:09 +0200

    Nesting Functions 1 - (Js Guide)

--- main.js
+++ main.js
@@ -1,29 +1,20 @@
-// Functions 
-//function sayHello() {
-//console.log("Hello World");
-//}
+// Nested Functions
+function sayHi() {
+  console.log("Hi");
 
-// sayHello();
+  function sayHello() {
+    console.log("Hello");
 
-//function addTwoNumbers(num1, num2) {
-  //console.log(num1 + num2);
-//}
-
-//addTwoNumbers(5,10);
-
-
-//let randomNumber = Math.random();
-//console.log(randomNumber);
-
-function addTwoNumbers(num1, num2) {
-  return num1 + num2;
+    function sayWorld() {
+      console.log("World");
+    }
+    sayWorld();
+  }
+  sayHello();
 }
 
-//console.log(addTwoNumbers(5,10));
-let sum = addTwoNumbers(2,4);
-let sum2 = addTwoNumbers(5,6);
-console.log(sum);
-console.log(sum2);
+sayHi();
+
 
 
 
