a40edf9f4d57f5733f36e78d49dd46d7c5b9c414
Bart <akiraenishi@gmail.com>
2024-10-15 21:39:57 +0200

    module arrow functions - E6 (Guide Js)

--- index.js
+++ index.js
@@ -1,19 +1,43 @@
+// arrow function
 
-const person = { 
-  name: "Mosh",
-  walk() {
-    console.log(this);
-  }
-};
+// old way
+const square = function(number) {
+  return number * number;
+}
 
-person.walk();
+// new way
+const square2 = (number) => {
+  return number * number;
+} 
+
+// shorter way
+const square3 = (number) => number * number;
+
+console.log(square(3));
+
+// filter
+const jobs = [
+  { id: 1, isActive: true },
+  { id: 2, isActive: true },
+  { id: 3, isActive: false },
+];
+
+// old way
+const activeJobs = jobs.filter(function(job) {
+  return job.isActive;
+});
+
+// new way
+const activeJobs2 = jobs.filter((job) => job.isActive);
 
 
-const walk = person.walk.bind(person);
-walk();
 
-// function js are objects has properties and methods you can use 
-// bind method is used to bind a function to an object
+
+
+
+
+
+
 
 
 
