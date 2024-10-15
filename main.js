e2e3f9da463003863892b622c3cfd27f83461346
Bart <akiraenishi@gmail.com>
2024-10-15 11:54:48 +0200

    Math functions (Js Guide)

--- main.js
+++ main.js
@@ -1,25 +1,15 @@
-// Handling and throwing errors
-
-/*console.log(myName);*/
-// not defined error
-
-throw new Error("Something went wrong..."); 
-console.log();
-
-// try catch finally
-
-try {
-  throw new Error("Something went wrong...");
-} catch (error) {
-  console.log(error);
-} finally {
-   console.log("This will always run!");
-}
-
-console.log("hello");
+// Math functions
+console.log(Math.abs(-5));
+// math abs (work with positive numbers regardsless of the orginal assinged value)
+// Math ceil rounds up
+// Math floor rounds down
+// math round rounds up or down
+// math max returns the highest value
+// math min returns the lowest value
+// math random returns a random number between 0 and 1
 
 
-
+ 
 
 
 
